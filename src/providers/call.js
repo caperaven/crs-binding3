bd3fc5fc28e7d40e95852dc5c865a57c358641e5
caperaven <caperaven@gmail.com>
2022-11-22 23:48:12 +0200

    initial

--- src/providers/call.js
+++ src/providers/call.js
@@ -24,6 +24,7 @@ export default class CallProvider {
         }
 
         this.#events[event][attr.ownerElement.dataset.uuid] = attr.value;
+        attr.ownerElement.removeAttribute(attr.name);
     }
 
     /**
