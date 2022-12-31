f5389a4a9b72401481a0699400e1ba123b2e0ea0
caperaven <caperaven@gmail.com>
2022-12-31 18:57:50 +0200

    for binding updating ui on array change

--- src/utils/dom-collection-manager.js
+++ src/utils/dom-collection-manager.js
@@ -1,7 +1,13 @@
 class DomCollection {
-    static append(uuid, item, template, inflationFn) {
+    static append(uuid, item) {
         const element = crs.binding.elements[uuid];
         if (element == null) return;
+
+        const details = crs.binding.inflation.store.get(uuid);
+
+        const instance = details.template.content.cloneNode(true);
+        details.fn(instance, item);
+        element.appendChild(instance);
     }
 }
 
