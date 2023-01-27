735670eb4d53b5c7ee026a8e185d4e217c80e377
caperaven <caperaven@gmail.com>
2023-01-27 13:10:38 +0200

    add comments to providers

--- src/providers/properties/bind.js
+++ src/providers/properties/bind.js
@@ -1,17 +1,35 @@
 import "../../expressions/code-factories/if.js";
 
+/**
+ * @class BindProvider - Binds an element to a property of a data object
+ *
+ * @example
+ * <input value.bind="person.firstName">
+ */
 export default class BindProvider {
     #store = {};
     #onEventHandler = this.#onEvent.bind(this);
 
+    /**
+     * @constructor - Adds a change event listener to the document
+     */
     constructor() {
         document.addEventListener("change", this.#onEventHandler);
     }
 
+    /**
+     * @property store - The store of element uuids and their properties to update
+     * @returns {{}}
+     */
     get store() {
         return this.#store;
     }
 
+    /**
+     * @method #onEvent - The event handler for the change event
+     * @param event
+     * @returns {Promise<void>}
+     */
     async #onEvent(event) {
         const bid = event.target["__bid"];
         const field = event.target.dataset.field;
@@ -21,6 +39,12 @@ export default class BindProvider {
         await crs.binding.data.setProperty(bid, field, event.target.value);
     }
 
+    /**
+     * @method parse - Parses the element and adds a change event listener to it and sets the data-field attribute
+     * @param attr {Attr} - The attribute to parse
+     * @param context {BindingContext} - The context of the element
+     * @returns {Promise<void>}
+     */
     async parse(attr, context) {
         const parts = attr.name.split(".");
         const element = attr.ownerElement;
@@ -37,6 +61,12 @@ export default class BindProvider {
         crs.binding.data.setCallback(element["__uuid"], context.bid, [path]);
     }
 
+    /**
+     * @method update - Updates the element with the value of the property
+     * @param uuid {string} - The uuid of the element
+     * @param properties {string[]} - The properties to update
+     * @returns {Promise<void>}
+     */
     async update(uuid, ...properties) {
         const element = crs.binding.elements[uuid];
         if (element == null) return;
@@ -51,6 +81,11 @@ export default class BindProvider {
         }
     }
 
+    /**
+     * @method clear - Clears the element from the store
+     * @param uuid {string} - The uuid of the element
+     * @returns {Promise<void>}
+     */
     async clear(uuid) {
         delete this.#store[uuid];
     }
