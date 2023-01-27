87892f331b865d1f0c6e69a21f3284284b575d0b
caperaven <caperaven@gmail.com>
2023-01-27 13:26:26 +0200

    add comments to providers

--- src/store/template-inflation-store.js
+++ src/store/template-inflation-store.js
@@ -1,16 +1,35 @@
+/**
+ * @class TemplateInflationStore - This is a store used for inflation of templates.
+ * This is used internally to store templates and functions to be called when a template is inflated.
+ */
 export class TemplateInflationStore {
     #store = {};
 
+    /**
+     * @method add - Add a template to the store with a function to be called when the template is inflated.
+     * @param name {string} - The name of the template
+     * @param template {string} - The template to be stored
+     * @param fn {function} - The function to be called when the template is inflated
+     */
     add(name, template, fn) {
         this.#store[name] = {
             template, fn
         }
     }
 
+    /**
+     * @method get - Get a template from the store by name and return the template and function to be called when the template is inflated.
+     * @param name {string} - The name of the template
+     * @returns {*}
+     */
     get(name) {
         return this.#store[name];
     }
 
+    /**
+     * @method remove - Remove a template from the store by name
+     * @param name {string} - The name of the template
+     */
     remove(name) {
         const item = this.#store[name];
         delete this.#store[name];
