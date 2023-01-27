5df56f7449f073a54d05fba506c7e030a793ca21
caperaven <caperaven@gmail.com>
2023-01-27 13:43:46 +0200

    add comments to providers

--- src/utils/dom-collection-manager.js
+++ src/utils/dom-collection-manager.js
@@ -1,4 +1,16 @@
+/**
+ * @class DomCollection - A class that manages the DOM collection as if it s an array.
+ * Though this file is not loaded by default, it is imported by the array proxy.
+ * This is used internally to update UI elements when an array is modified but can also be used externally.
+ *
+ * The methods match that of arrays, but the first parameter is the uuid of the element to update.
+ */
 class DomCollection {
+    /**
+     * @method append - Append items to the end of the collection.
+     * @param uuid {string} - The uuid of the element to update.
+     * @param items - The collection of items to append.
+     */
     static append(uuid, ...items) {
         const element = crs.binding.elements[uuid];
         if (element == null) return;
@@ -16,6 +28,13 @@ class DomCollection {
         element.appendChild(fragment);
     }
 
+    /**
+     * @method splice - Remove and add items to the collection.
+     * @param uuid {string} - The uuid of the element to update.
+     * @param start {number} - The index to start at.
+     * @param deleteCount {number} - The number of items to remove.
+     * @param items - The collection of items to add.
+     */
     static splice(uuid, start, deleteCount, ...items) {
         const element = crs.binding.elements[uuid];
         if (element == null) return;
@@ -43,6 +62,10 @@ class DomCollection {
         element.insertBefore(fragment, target);
     }
 
+    /**
+     * @method pop - Remove the last item from the collection.
+     * @param uuid {string} - The uuid of the element to update.
+     */
     static pop(uuid) {
         const element = crs.binding.elements[uuid];
         if (element == null) return;
@@ -52,6 +75,10 @@ class DomCollection {
         }
     }
 
+    /**
+     * @method shift - Remove the first item from the collection.
+     * @param uuid {string} - The uuid of the element to update.
+     */
     static shift(uuid) {
         const element = crs.binding.elements[uuid];
         if (element == null) return;
