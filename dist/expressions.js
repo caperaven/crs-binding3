462fe5fab6d03dccf9b1d9c460904d2e227e2740
caperaven <caperaven@gmail.com>
2023-06-28 10:27:12 +0200

    update dist

--- dist/expressions.js
+++
@@ -1,6 +0,0 @@
-import { sanitize } from "./expressions/exp-sanitizer.js";
-import { translateFactory } from "./expressions/code-factories/translate.js";
-export {
-  sanitize,
-  translateFactory
-};
--- dist/providers/element/template.js
+++
@@ -1,8 +0,0 @@
-class TemplateProvider {
-  async parse(element, context) {
-    await crs.binding.templateProviders.executeTemplateAction(element, context);
-  }
-}
-export {
-  TemplateProvider as default
-};
