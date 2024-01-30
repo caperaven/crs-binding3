f81f9e9cda2bd8f8fcd6c8de20a72b20dd42dfc4
caperaven <caperaven@gmail.com>
2024-01-30 10:47:20 +0200

    add custom event dispatch for components

--- app/custom-events/custom-events.js
+++ app/custom-events/custom-events.js
@@ -1,7 +1,9 @@
-import "./custom-component.js";
+import "./input-mimic/input-mimic.js";
+import "./custom-event/custom-event.js";
 
 export default class CustomEventsViewModel extends crs.classes.ViewBase {
     preLoad() {
         this.setProperty("customValue", "Custom Events Value 1");
+        this.setProperty("customValue2", "Custom Events Value 2");
     }
 }
